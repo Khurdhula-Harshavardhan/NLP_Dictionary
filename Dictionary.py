c0af65f3a7d5ed56ceecd33894dc6499bea248c4
Harsha <khurdhulaharshavardhan@gmail.com>
2023-01-15 23:21:45 -0500

    refactors process_regex() method to add modularity.

--- Dictionary.py
+++ Dictionary.py
@@ -11,7 +11,9 @@ class Dictionary():
     text= None
     log_handler = None
     CURRENT_PATH = None
-
+    #total_words_replaced
+    #total_characters_discarded
+    
     #Constructor sets up all the global variables.
     def __init__(self) -> None:
 
@@ -38,32 +40,56 @@ class Dictionary():
         """
         We shall use the lib "os" module, which provides us with a method: os.getcwd(),
         to eliminate any possible directory/file path errors with the read of the file.
+
+        However, we are setting up the file_handlers within this method rather than the constructor, 
+        because the first method to be called, while executing this script is:
+        Dictionary.process_regex(filename) 
+        Hence the parameter is passed here.
         """
+
         try:
             self.file_handler = open(self.CURRENT_PATH + filename, "r", encoding = "UTF-8")
             self.log_handler = open(self.CURRENT_PATH+ "changes_log.txt", "w+", encoding="UTF-8")
-            for line in self.file_handler:
+            self.text = self.file_handler.readlines()
+
+            self.replace_british_words()
+        except Exception as e:
+            print("The following error occured while trying to read the file: " + str(e))
+       
+    def replace_british_words(self) -> None:
+        """
+        Dictionary.replace_british_words() aims to replace words captured by patterns in the dict we have, with counter part American words.
+        This is achieved by using re.sub() method.
+        The change made is stored to the global variable self.text which is a list that stores all lines read from the file.
+        The changes are also noted to changes_log.txt
+        """
+
+        try:
+            for line in self.text:
                 for patternn in self.british_to_american_text_patterns.keys():
                     before_change = line
                     after_change = re.sub(patternn, self.british_to_american_text_patterns[patternn], line)
+
                     if before_change != after_change:
                         self.write_log("Replacing British Words", before_change, after_change)
-                        print(before_change)
-                        print(after_change)
-                        
 
-            
-
+                    line = after_change
+                    
         except Exception as e:
-            print("The following error occured while trying to read the file: " + str(e))
-
+            print(str(e))
     
 
-    def write_log(self, calling_method, original_text, modified_text):
+    def write_log(self, calling_method, original_text, modified_text) -> None:
+        """
+        write_log method, writes three statements to changes_log.txt everytime it is called,
+        the goal of this method is to capture the changes that occur in sentences as we process the lines from self.text.
+        """
+
         try:
-            line = "\n" + calling_method + "\n" + original_text + "\n" + modified_text + "\n"
+            line = "\n" + calling_method + "\n" + original_text + modified_text + "\n"
             self.log_handler.write(line)
+
         except Exception as e:
             print("The following error occured while trying to write changes to changes_log.txt: " + str(e))
-            
+
 Dictionary().process_regex("theWaroftheWorlds.txt")
